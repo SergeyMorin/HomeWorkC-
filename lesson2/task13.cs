fc9f511aea258b732d08ccb65aafc9e11b93bff3
SergeyMorin <116893928+SergeyMorin@users.noreply.github.com>
2022-12-02 22:53:15 +0300

    update

--- lesson2/task13.cs
+++ lesson2/task13.cs
@@ -1,1 +1,27 @@
-123123
+// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число больше 99");
+string num1 = Console.ReadLine()!;
+
+if (num1[0] == '-')
+{
+    if (num1.Length >= 4)
+    {
+        Console.WriteLine(num1[3]);
+    }
+    else
+    {
+        Console.WriteLine("число менее трех знаков");
+    }
+}
+else
+{
+    if (num1.Length >= 3)
+    {
+        Console.WriteLine(num1[2]);
+    }
+    else
+    {
+        Console.WriteLine("число менее трех знаков");
+    }
+}
