56ef77fd6ea07802fb9d9e02b2f4e3c8e660e9cd
SergeyMorin <116893928+SergeyMorin@users.noreply.github.com>
2022-12-02 22:50:43 +0300

    update

--- lesson2/task10.cs
+++ lesson2/task10.cs
@@ -1,1 +1,14 @@
-213123
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает ВТОРУЮ цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int num1 = int.Parse(Console.ReadLine()!);
+if ((num1 >= 100) && (num1 < 1000))
+{
+    int firstDigit = num1 % 100;
+    int secondDigit = firstDigit / 10;
+    Console.WriteLine(secondDigit);
+}
+else
+{
+    Console.WriteLine("Введено не трех значное число");
+}
