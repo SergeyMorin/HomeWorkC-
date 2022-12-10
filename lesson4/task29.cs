52342a63eba80410d0169941008a7ea280f34a1a
SergeyMorin <serega-morin@yandex.ru>
2022-12-10 19:51:25 +0300

    update

--- lesson4/task29.cs
+++ lesson4/task29.cs
@@ -1,16 +1,20 @@
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-Console.Write("Массив из скольки элементов требуется сгенерировать?");
+Console.Write("Массив из скольки элементов требуется сгенерировать? ");
 int n = int.Parse(Console.ReadLine()!);
+Console.Write("Чему равен минимальный элемент массива? ");
+int mininput = int.Parse(Console.ReadLine()!);
+Console.Write("Чему равен максимальный элемент массива? ");
+int maxinput = int.Parse(Console.ReadLine()!);
 
-method(n);
+method(n, mininput, maxinput);
 
-void method(int i)
+void method(int i, int min, int max)
 {
 int[] array = new int[i];
 while(i>0)
 {
-    array[i - 1] = new Random().Next(0,100);
+    array[i - 1] = new Random().Next(min,max+1);
     i--;
 }
 Console.Write("[");
