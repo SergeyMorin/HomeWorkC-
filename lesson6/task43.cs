10e2ef1cc071cc8da125b8983d5b3488f3664057
SergeyMorin <serega-morin@yandex.ru>
2022-12-20 01:57:50 +0300

    update

--- lesson6/task43.cs
+++ lesson6/task43.cs
@@ -1,33 +1,42 @@
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
 //  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
 double[,] arrayuserinput = new double[2, 4];
-Console.WriteLine("Введите коэфиценты k1 ,b1 ,k2 ,b2: ");
-for (int i = 0; i < arrayuserinput.GetLength(0); i++)
+while (true)
 {
-    for (int j = 0; j < arrayuserinput.GetLength(1); j++)
+    Console.WriteLine("Введите коэфиценты k1 ,b1 ,k2 ,b2: ");
+    for (int i = 0; i < arrayuserinput.GetLength(0); i++)
     {
-        if (j % 2 == 0)
+        for (int j = 0; j < arrayuserinput.GetLength(1); j++)
         {
-            continue;
+            if (j % 2 == 0)
+            {
+                continue;
+            }
+            else
+                arrayuserinput[i, j] = double.Parse(Console.ReadLine()!);
         }
-        else
-            arrayuserinput[i, j] = double.Parse(Console.ReadLine()!);
     }
+    if (arrayuserinput[0, 1] == arrayuserinput[1, 1])
+        Console.WriteLine("Некорректный ввод, k1=k2 прямые параллельны. Повторите попытку");
+    else
+        break;
 }
 
-for (int x = 0; x < XY(arrayuserinput).Length; x++)
-{
-    Console.WriteLine(XY(arrayuserinput)[x]);
-}
+XY(arrayuserinput);
 
 double[] XY(double[,] array)
 {
-    array[0, 2] = (array[0, 3] - array[1, 3]) / (array[1, 1] - array[0, 1]);
-    array[1, 2] = array[0, 2];
-    array[0, 0] = array[0, 1] * array[0, 2] + array[0, 3];
-    array[1, 0] = array[1, 1] * array[0, 2] + array[1, 3];
-    double[] point = { array[0, 2], array[0, 0] };
-    return point;
+    {
+        array[0, 2] = (array[0, 3] - array[1, 3]) / (array[1, 1] - array[0, 1]);
+        array[1, 2] = array[0, 2];
+        array[0, 0] = array[0, 1] * array[0, 2] + array[0, 3];
+        array[1, 0] = array[1, 1] * array[0, 2] + array[1, 3];
+        double[] point = new double[2] { array[0, 2], array[0, 0] };
+        for (int x = 0; x < point.Length; x++)
+        {
+            Console.Write($"{point[x]} ");
+        }
+        return point;
+    }
 }
