3de26a1f08d822ebb59d069f05add8debf8c90a2
SergeyMorin <serega-morin@yandex.ru>
2022-12-20 02:17:25 +0300

    apdate 43

--- lesson6/task43.cs
+++ lesson6/task43.cs
@@ -1,31 +1,9 @@
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
 //  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-double[,] arrayuserinput = new double[2, 4];
-while (true)
-{
-    Console.WriteLine("Введите коэфиценты k1 ,b1 ,k2 ,b2: ");
-    for (int i = 0; i < arrayuserinput.GetLength(0); i++)
-    {
-        for (int j = 0; j < arrayuserinput.GetLength(1); j++)
-        {
-            if (j % 2 == 0)
-            {
-                continue;
-            }
-            else
-                arrayuserinput[i, j] = double.Parse(Console.ReadLine()!);
-        }
-    }
-    if (arrayuserinput[0, 1] == arrayuserinput[1, 1])
-        Console.WriteLine("Некорректный ввод, k1=k2 прямые параллельны. Повторите попытку");
-    else
-        break;
-}
+СalculationXY(BKUserInput());
 
-XY(arrayuserinput);
-
-double[] XY(double[,] array)
+double[] СalculationXY(double[,] array)
 {
     {
         array[0, 2] = (array[0, 3] - array[1, 3]) / (array[1, 1] - array[0, 1]);
@@ -38,5 +16,30 @@ double[] XY(double[,] array)
             Console.Write($"{point[x]} ");
         }
         return point;
+    }
+}
+
+double[,] BKUserInput()
+{
+    double[,] arrayuserinput = new double[2, 4];
+    while (true)
+    {
+        Console.WriteLine("Введите коэфиценты k1 ,b1 ,k2 ,b2: ");
+        for (int i = 0; i < arrayuserinput.GetLength(0); i++)
+        {
+            for (int j = 0; j < arrayuserinput.GetLength(1); j++)
+            {
+                if (j % 2 == 0)
+                {
+                    continue;
+                }
+                else
+                    arrayuserinput[i, j] = double.Parse(Console.ReadLine()!);
+            }
+        }
+        if (arrayuserinput[0, 1] == arrayuserinput[1, 1])
+            Console.WriteLine("Некорректный ввод, k1=k2 прямые параллельны. Повторите попытку");
+        else
+            return arrayuserinput;
     }
 }
