1cb9180036bd3adbadc4449fc1c2be0b95e35dd7
SergeyMorin <serega-morin@yandex.ru>
2022-12-26 00:09:36 +0300

    update

--- lesson8/task58.cs
+++ lesson8/task58.cs
@@ -5,63 +5,68 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+Console.WriteLine("Матрицу A из скольки строк M и столбцов N создать?\n Введите M: ");
+int m1UserInput = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите N:");
+int n1UserInput = int.Parse(Console.ReadLine()!);
 
-int [,] matrix1 = GenerateMatrixA ();
+Console.WriteLine("Матрицу B из скольки строк M и столбцов N создать?\n Введите M: ");
+int m2UserInput = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите N:");
+int n2UserInput = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int[,] matrix1 = GenerateMatrix(m1UserInput, n1UserInput);
 PrintMatrix(matrix1);
 Console.WriteLine();
-int [,] matrix2 = GenerateMatrixB ();
+int[,] matrix2 = GenerateMatrix(m2UserInput, n2UserInput);
 PrintMatrix(matrix2);
 Console.WriteLine();
-PrintMatrix(ProductMatrix (matrix1, matrix2));
-
-int[,] GenerateMatrixA ()
+if (m1UserInput == n2UserInput)
 {
-    int[,] matrix = new int[2,3];
-        for (int m = 0 ; m < matrix.GetLength(0) ; m++)
-        {
-            for (int n = 0 ; n < matrix.GetLength(1) ; n++)
-            {
-                matrix[m, n] = new Random().Next(1,10);
-            }
-        }
-    return matrix;  
+    Console.WriteLine("Произведением матрицы A на матриуц B будет матрица AB:");
+    PrintMatrix(ProductMatrix(matrix1, matrix2));
+}
+else
+{
+    Console.WriteLine("данные матрицы незьзя умножить.");
 }
 
-int[,] GenerateMatrixB ()
+int[,] GenerateMatrix(int m, int n)
 {
-    int[,] matrix = new int[3,2];
-        for (int m = 0 ; m < matrix.GetLength(0) ; m++)
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            for (int n = 0 ; n < matrix.GetLength(1) ; n++)
-            {
-                matrix[m, n] = new Random().Next(1,10);
-            }
+            matrix[i, j] = new Random().Next(1, 10);
         }
-    return matrix;  
+    }
+    return matrix;
 }
-int [,] ProductMatrix (int [,] MatrixA, int[,]MatrixB)
+
+int[,] ProductMatrix(int[,] MatrixA, int[,] MatrixB)
 {
-    int [,] productMatrixAB = new int [MatrixA.GetLength(0),MatrixB.GetLength(1)];
+    int[,] productMatrixAB = new int[MatrixA.GetLength(0), MatrixB.GetLength(1)];
     for (int i = 0; i < MatrixA.GetLength(0); i++)
     {
         for (int j = 0; j < MatrixB.GetLength(1); j++)
         {
-                for (int n = 0; n < MatrixA.GetLength(1); n++)
-                {
-                    productMatrixAB[i,j] += MatrixA[i,n] * MatrixB[n,j];
-                }
+            for (int n = 0; n < MatrixA.GetLength(1); n++)
+            {
+                productMatrixAB[i, j] += MatrixA[i, n] * MatrixB[n, j];
+            }
         }
     }
     return productMatrixAB;
 }
 
-void PrintMatrix (int [,] Matrix)
+void PrintMatrix(int[,] Matrix)
 {
     for (int i = 0; i < Matrix.GetLength(0); i++)
     {
         for (int j = 0; j < Matrix.GetLength(1); j++)
         {
-            Console.Write(Matrix[i,j]+" ");
+            Console.Write(Matrix[i, j] + " ");
         }
         Console.WriteLine();
     }
