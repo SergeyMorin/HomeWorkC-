2bacb108028f86e7b70e9f773b879e816d4b24ec
SergeyMorin <serega-morin@yandex.ru>
2022-12-26 10:02:19 +0300

    task 60

--- lesson8/task60.cs
+++ lesson8/task60.cs
@@ -4,3 +4,54 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+
+Console.WriteLine("Введите M: ");
+int aSide = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите N: ");
+int bSide = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите K: ");
+int cSide = int.Parse(Console.ReadLine()!);
+
+if (aSide * bSide * cSide > 90)
+    Console.WriteLine("такой трехмерный массив не возможно заполнить двух значными не повтарающимеся числами");
+else
+{
+    Print3DMatrix(Matrix3d(aSide, bSide, cSide));
+}
+
+int[,,] Matrix3d(int a, int b, int c)
+{
+    bool[] array = new bool[100];
+    int[,,] matrix = new int[a, b, c];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(10, 100);
+                if (!array[matrix[i, j, k]])
+                    array[matrix[i, j, k]] = true;
+                else
+                    k--;
+            }
+        }
+    }
+    return matrix;
+}
+
+void Print3DMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write(matrix[i, j, k] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
